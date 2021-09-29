1ad8ee6b2f9c0322139742bbf3cd6b35f5b9ce18
Sean Yeoh <sean.yeoh@hotmail.com>
2021-09-30 05:37:17 +0800

    Fix url redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/tailwind-alpinejs/docs/intro" />;
+  return <Redirect to="/tailwind-alpinejs/docs/modal" />;
 }
